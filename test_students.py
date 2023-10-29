b7f07e760983a9515409b3e5b919f34664227cf8
Kay13051976 <kaywanwisa4@googlemail.com>
2023-10-29 08:15:40 +0000

    Create cladd TestStudent and method test_full_name to test full_name method

--- test_students.py
+++ test_students.py
@@ -3,10 +3,9 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
-
-
-
-
+    def test_full_name(self):
+        student = Student("John", "Doe")
+        self.assertEqual(student.full_name, "John Doe")
 
 
 if __name__ == '__main__':
