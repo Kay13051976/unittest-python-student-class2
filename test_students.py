6495167236aaedad04af2f047708540876029f9f
Kay13051976 <kaywanwisa4@googlemail.com>
2023-10-30 21:09:59 +0000

    Add doc string and and create apply_extension method

--- test_students.py
+++ test_students.py
@@ -1,5 +1,6 @@
 import unittest
 from student import Student
+from datetime import timedelta
 
 
 class TestStudent(unittest.TestCase):
@@ -43,7 +44,15 @@ class TestStudent(unittest.TestCase):
     def test_apply_extension(self):
         old_end_date = self.student.end_date
         self.student.apply_extension(5)
+        self.assertEqual(self.student.end_date, old_end_date + timedalta(days=5))
+        """
+        The method below is also great! But keep in mid that it will
+        only be correct if a student has received 1 extension. If they
+        receive a second - it  would return false
 
+        self.student.apply_extension(5
+        self.assertEqual(self.student.end_date, self.student._start_date + timedelta(days=370))
+        """
 
 if __name__ == '__main__':
     unittest.main()
