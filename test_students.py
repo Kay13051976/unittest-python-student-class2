81225d1977886d27b0a75ca3dd69e7ca13e90cda
Kay13051976 <kaywanwisa4@googlemail.com>
2023-10-30 20:41:30 +0000

    Add doc strin about the step to test the method test_apply_extension

--- test_students.py
+++ test_students.py
@@ -3,6 +3,16 @@ from student import Student
 
 
 class TestStudent(unittest.TestCase):
+    """
+    * Create a new test mothod called test_apply_extension
+    * Inside test_apply_extension, store the current end_date for our student instance in a varible called old_end_date
+    * Call a method maned apply_extension that will take anumber of days as an argument on the student instance to update the end_date
+    * Assert whether the instance's end_date equals the old end date plus the days plus the days argument that was passed in using timedelta
+    * Run the tests to confirm that the new method is failing
+    * In the Student class, created a new method called apply_extension that has a parameter called days
+    * Use the timedelta method from datetime to update the end_date property
+    * Run the tests to confirm they are working.
+    """
     @classmethod
     def setUpClass(cls):
         print("setUpClass")
@@ -10,7 +20,7 @@ class TestStudent(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         print("tearDownClass")
-        
+
     def setUp(self):
         print("setUp")
         self.student = Student("John", "Doe")
